dbd18b4ec2521fff5a668667fae5c6f69e91af02
Corpa2 <>
2026-04-13 17:52:19 -0500

    Repositories creation

---
+++ actividad04/actividad04/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using actividad04.Models;
+using actividad04.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace actividad04.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly IGenericRepository<Product> _productRepo;
+
+    public ProductController(IGenericRepository<Product> productRepo)
+    {
+        _productRepo = productRepo;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetProducts()
+    {
+        var products = await _productRepo.GetAllAsync();
+        return Ok(products);
+    }
+    
+    
+}
